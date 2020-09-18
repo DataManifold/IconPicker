3666d0f111a9487f377dfd07dabe6d00f790ebc1
Shean Bjoralt <shean@datamanifold.com>
2020-09-18 15:32:21 -0600

    initial commit

---
+++ IconPicker/IconPicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IconPicker
+//
+//  Created by Shean Bjoralt on 9/18/20.
+//  Copyright © 2020 Shean Bjoralt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
